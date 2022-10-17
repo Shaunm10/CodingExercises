7ee48056656782d4a20ddd0338ae85cafd2d20af
Shaun Sargent <ssargent@costar.com>
2022-10-17 14:53:54 -0400

    Added finish SortedSquaredArray

--- SortedSquaredArray/Exercise.cs
+++ SortedSquaredArray/Exercise.cs
@@ -7,11 +7,40 @@ namespace CodingExercises.SortedSquaredArray
         integers also sorted in ascending order.
         */
 
+        public static int[] SortedSquaredArrayLinq(int[] array)
+        {
+       
+            return array
+                .Select(x => x * x)
+                .OrderBy(x =>x)
+                .ToArray();
+        }
+
         public static int[] SortedSquaredArray(int[] array)
         {
-            return
-            // Write your code here.
-            return new int[] { };
+            var arrayLength = array.Length;
+            var squaredArray = new int[arrayLength];
+            var startPointer = 0;
+            var endPointer = arrayLength;
+            
+            for(int i = 0; i < arrayLength; i++)
+            {
+                var valueAtBegin = array[startPointer];
+                var valueAtEnd = array[endPointer-1];
+                
+                if(Math.Abs(valueAtBegin) >= Math.Abs(valueAtEnd))
+                {
+                    squaredArray[(arrayLength-1)-i] = valueAtBegin * valueAtBegin;
+                    startPointer++;
+                }
+                else
+                {     
+                    squaredArray[(arrayLength-1)-i] = valueAtEnd * valueAtEnd;
+                    endPointer--;
+                }
+            }
+
+            return squaredArray;
         }
     }
 }
--- SortedSquaredArray/Test.cs
+++ SortedSquaredArray/Test.cs
@@ -17,7 +17,82 @@ namespace CodingExercises.SortedSquaredArray
 
             // assert:
             result.Should().NotBeNull();
-            result.Should().BeEquivalentTo(expectedOutput);
+            result.Should().ContainInOrder(expectedOutput);
+        }
+
+          [Fact]
+        public void Sample_Test2()
+        {
+            // arrange:
+            var array = new int[] { -2, -1 };
+            var expectedOutput = new int[] { 1, 4};
+
+            // act:
+            var result = Exercise.SortedSquaredArray(array);
+
+            // assert:
+            result.Should().NotBeNull();
+            result.Should().ContainInOrder(expectedOutput);
+        }
+
+        [Fact]
+        public void Sample_Test3()
+        {
+            // arrange:
+            var array = new int[] { -3, -2, -1};
+            var expectedOutput = new int[] { 1, 4, 9};
+
+            // act:
+            var result = Exercise.SortedSquaredArray(array);
+
+            // assert:
+            result.Should().NotBeNull();
+            result.Should().ContainInOrder(expectedOutput);
+        }
+    
+        [Fact]
+        public void Sample_Test4()
+        {
+              // arrange:
+            var array = new int[] { -5, -4, -3, -2, -1};
+            var expectedOutput = new int[] { 1, 4, 9, 16, 25};
+
+            // act:
+            var result = Exercise.SortedSquaredArray(array);
+
+            // assert:
+            result.Should().NotBeNull();
+            result.Should().ContainInOrder(expectedOutput);
+        }
+
+         [Fact]
+        public void Sample_Test5()
+        {
+              // arrange:
+            var array = new int[] { -2, -1};
+            var expectedOutput = new int[] {1, 4};
+
+            // act:
+            var result = Exercise.SortedSquaredArray(array);
+
+            // assert:
+            result.Should().NotBeNull();
+            result.Should().ContainInOrder(expectedOutput);
+        }
+
+         [Fact]
+        public void Sample_Test6()
+        {
+              // arrange:
+            var array = new int[] { -1, -1, 2, 3, 3, 3, 4};
+            var expectedOutput = new int[] {1, 1, 4, 9, 9, 9, 16};
+
+            // act:
+            var result = Exercise.SortedSquaredArray(array);
+
+            // assert:
+            result.Should().NotBeNull();
+            result.Should().ContainInOrder(expectedOutput);
         }
     }
 }
