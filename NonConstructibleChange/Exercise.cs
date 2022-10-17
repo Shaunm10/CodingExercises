a1567a6cce6d471509c21f998fcf8c49dcf4b32f
Shaun Sargent <ssargent@costar.com>
2022-10-17 17:17:51 -0400

    -WIP

--- NonConstructibleChange/Exercise.cs
+++ NonConstructibleChange/Exercise.cs
@@ -2,9 +2,62 @@ namespace CodingExercises.NonConstructibleChange;
 
 public class Exercise
 {
+    /*
+        Given an array of positive integers representing the values of coins in your possession, write a function 
+        that returns the minimum amount of change (the minimum sum of money) that you cannot create. 
+        The given coins can have any positive integer value and aren't necessarily unique 
+        (i.e., you can have multiple coins of the same value).
+
+        For example, if you're given coins = [1, 2, 5], the minimum amount of change that you can't create is 4. 
+        If you're given no coins, the minimum amount of change that you can't create is 1.
+    */
+
     public static int NonConstructibleChange(int[] coins)
     {
-        // Write your code here.
-        return -1;
+        // first sort the coins
+        var sortedCoins = coins.OrderBy(x => x);
+
+        var coinPermutations = GetPermutations(coins);
+        var amountsCollected = coinPermutations
+            .Select(x => x.Sum(c => c))
+            .OrderBy(x => x)
+            .ToList();
+
+        var counter = 1;
+        var amountFound = 0;
+        while (counter < amountsCollected.Count())
+        {
+            // get the amount
+            var amount = amountsCollected.FirstOrDefault(c => c == counter);
+
+            if (amount != 0)
+            {
+                amountFound = amount;
+            }
+            else
+            {
+                break;
+            }
+
+            // increment the counter.
+            counter++;
+        }
+
+        return counter;
+    }
+
+    private static List<List<int>> GetPermutations(int[] coins)
+    {
+        var variants = new List<List<int>>();
+        for (int i = 0; i < coins.Length; i++)
+        {
+            variants.Add(coins.Take(i + 1).ToList());
+        }
+
+        if (coins.Length > 1)
+        {
+            variants.AddRange(GetPermutations(coins.Skip(1).ToArray()));
+        }
+        return variants;
     }
 }
--- NonConstructibleChange/Tests.cs
+++ NonConstructibleChange/Tests.cs
@@ -4,8 +4,24 @@ namespace CodingExercises.NonConstructibleChange;
 
 public class Tests
 {
+
     [Fact]
     public void Sample_Test()
+    {
+        // arrange:
+        var coins = new int[] {1,2,5};
+
+        var expectedOutcome = 4;
+
+        // act:
+        var response = Exercise.NonConstructibleChange(coins);
+
+        // assert:
+        response.Should().Be(expectedOutcome);
+    }
+
+    [Fact]
+    public void Sample_Test2()
     {
         // arrange:
         var coins = new int[] { 5, 7, 1, 1, 2, 3, 22 };
