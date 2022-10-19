78162758e6dcadfc426686b38e867aa6d6f9a233
Shaun Sargent <ssargent@costar.com>
2022-10-19 16:25:27 -0400

    - started off BranchSums

---
+++ BranchSums/BinaryTree.cs
@@ -0,0 +1,21 @@
+namespace CodingExercises.BranchSums
+{
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree()
+        {
+
+        }
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+}
---
+++ BranchSums/Tests.cs
@@ -0,0 +1,59 @@
+namespace CodingExercises.BranchSums
+{
+    public class Tests
+    {
+        public void Test1()
+        {
+            // arrange:
+            var targetTree = new BinaryTree
+            {
+                value = 1,
+                left = new BinaryTree
+                {
+                    value = 2,
+                    left = new BinaryTree
+                    {
+                        value = 4,
+                        left = new BinaryTree
+                        {
+                            value = 8
+                        },
+                        right = new BinaryTree
+                        {
+                            value = 9
+                        }
+                    },
+                    right = new BinaryTree
+                    {
+                        value = 5,
+                        left = new BinaryTree
+                        {
+                            value = 10
+                        }
+                    }
+
+                },
+                right = new BinaryTree
+                {
+                    value = 3,
+                    right = new BinaryTree
+                    {
+                        value = 7
+                    },
+                    left = new BinaryTree
+                    {
+                        value = 6
+                    }
+                }
+            };
+
+            var expectedOutput = new int[] { 15, 16, 18, 10, 11 };
+
+            // act:
+            var result = Exercise.BranchSums(targetTree);
+            result.Should().ContainInOrder(expectedOutput);
+
+            // assert:
+        }
+    }
+}
