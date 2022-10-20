19809cc1eab581401904e7ca03da5d027c30ae07
shaunsargent <ShaunM10@gmail.com>
2022-10-19 23:11:25 -0400

    - more wip

--- NodeDepths/Tests.cs
+++ NodeDepths/Tests.cs
@@ -2,6 +2,28 @@ namespace CodingExercises.NodeDepths
 {
     public class Tests
     {
-        
+        public void TestA()
+        {
+
+            // arrange:
+            var expectedResult = 16;
+            var tree = new BinaryTree
+            {
+                value = 1,
+                left = new BinaryTree
+                {
+
+                },
+                right = new BinaryTree
+                {
+
+                }
+            };
+
+            // act:
+            var result = Exercise.NodeDepths()
+
+            // assert:
+        }
     }
 }
