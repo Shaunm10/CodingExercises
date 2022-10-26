f4af37e97cd923b2451d5ff057d87b9d773be7ea
Shaun Sargent <ssargent@costar.com>
2022-10-26 17:16:02 -0400

    - working on BubbleSort

--- BubbleSort/Exercise.cs
+++ BubbleSort/Exercise.cs
@@ -12,8 +12,46 @@ namespace CodingExercises.BubbleSort
 
         public static int[] BubbleSort(int[] array)
         {
-            // Write your code here.
-            return new int[] { };
+            for (var start = 0; start < array.Count(); start++)
+            {
+                var wasSwapPerformed = false;
+                for (var end = start + 1; end < array.Count(); end++)
+                {
+                    var result = SwapIfNeeded(array, start, end);
+                    array = result.Array;
+
+                    // see if we did a swap, and if so we need to keep going.
+                    if (result.WasASwapPerformed)
+                    {
+                        wasSwapPerformed = true;
+                    }
+
+                }
+
+                if (!wasSwapPerformed)
+                {
+                    break;
+                }
+            }
+
+            return array;
+        }
+
+        public static (int[] Array, bool WasASwapPerformed) SwapIfNeeded(int[] array, int startIndex, int endIndex)
+        {
+            var firstItem = array[startIndex];
+            var secondItem = array[endIndex];
+            var wasASwapPerformed = false;
+
+            if (firstItem > secondItem)
+            {
+                // a swap is needed
+                wasASwapPerformed = true;
+                array[endIndex] = firstItem;
+                array[startIndex] = secondItem;
+            }
+
+            return (Array: array, WasASwapPerformed: wasASwapPerformed);
         }
     }
 }
