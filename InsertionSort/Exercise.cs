dfb6712c4acc39117d0988c189a2170f7c0e6962
Shaun Sargent <ssargent@costar.com>
2022-10-27 17:50:26 -0400

    - WIP

--- InsertionSort/Exercise.cs
+++ InsertionSort/Exercise.cs
@@ -4,8 +4,22 @@ namespace CodingExercises.InsertionSort
     {
         public static int[] InsertionSort(int[] array)
         {
+            int[] sortedList = new int[array.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                var movingCandidate = array[i];
+
+                int indexOfInsertion = GetIndexOfInsertion(sortedList, movingCandidate);
+
+            }
+
             // Write your code here.
             return new int[] { };
+        }
+
+        private static int GetIndexOfInsertion(int[] sortedList, int movingCandidate)
+        {
+            throw new NotImplementedException();
         }
     }
 }
