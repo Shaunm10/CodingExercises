fd7626d511daca7cb9e62b457e2d454bc0356127
Shaun Sargent <ssargent@costar.com>
2022-10-28 13:25:53 -0400

    Added basics for SelectionSort

---
+++ SelectionSort/Exercise.cs
@@ -0,0 +1,11 @@
+namespace CodingExercises.SelectionSort
+{
+    public class Exercise
+    {
+        public static int[] SelectionSort(int[] array)
+        {
+            // Write your code here.
+            return new int[] { };
+        }
+    }
+}
---
+++ SelectionSort/Tests.cs
@@ -0,0 +1,18 @@
+namespace CodingExercises.SelectionSort
+{
+    public class Tests
+    {
+        public void TestA()
+        {
+            // arrange:
+            var array = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+            var expectedOutput = new int[] { 2, 3, 5, 5, 6, 8, 9 };
+
+            // act:
+            var result = Exercise.SelectionSort(array);
+
+            // assert:
+            result.Should().ContainEquivalentOf(expectation);
+        }
+    }
+}
