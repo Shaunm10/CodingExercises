b1a6ebcfa7cfef28bd5bbc04298978960b8e33f4
Shaun Sargent <ssargent@costar.com>
2022-11-01 16:47:58 -0400

    SpiralTraverse WIP

--- SpiralTraverse/Exercise.cs
+++ SpiralTraverse/Exercise.cs
@@ -12,8 +12,59 @@ namespace CodingExercises.SpiralTraverse
         */
         public static List<int> SpiralTraverse(int[,] array)
         {
-            // Write your code here.
-            return new List<int>();
+            var items = new List<int>();
+            var processedItems = new List<Point>();
+            var xIndex = 0;
+            var yIndex = 0;
+            var direction = Direction.Left;
+
+            // assuming this is a square 2 dimensioal array.
+            var countOfOneSide = array.Length;
+            var totalCountOfAllItems = Math.Pow(countOfOneSide, 2);
+
+            for (int i = 0; i < totalCountOfAllItems; i++)
+            {
+                // get the number at these coordinates and add them to the list.
+                var numberAtIndexes = array[xIndex, yIndex];
+                items.Add(numberAtIndexes);
+
+                switch (direction)
+                {
+                    case Direction.Left:
+
+                        // can we keep going left?
+                        if (array.Length < yIndex)
+                        {
+
+                        }
+                        break;
+
+                    case Direction.Down:
+                        break;
+
+                    case Direction.Right:
+                        break;
+
+                    case Direction.Up:
+                        break;
+                }
+            }
+
+            return items;
         }
+    }
+
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public enum Direction
+    {
+        Left,
+        Down,
+        Right,
+        Up
     }
 }
--- SpiralTraverse/Tests.cs
+++ SpiralTraverse/Tests.cs
@@ -7,10 +7,10 @@ namespace CodingExercises.SpiralTraverse
         {
             // arrange:
             int[,] array = {
-                { 1, 2, 3, 4 },
+                { 1,   2,  3, 4 },
                 { 12, 13, 14, 5 },
                 { 11, 16, 15, 6 },
-                { 10, 9, 8, 7 }
+                { 10,  9,  8, 7 }
             };
 
             var expectedOutput = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
