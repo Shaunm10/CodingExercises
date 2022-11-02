58f391df6b95cc5006ff64b361688da3b73b3a33
Shaun Sargent <shaunm10@gmail.com>
2022-11-02 17:43:29 -0400

    - added shell for ArrayOfProducts

---
+++ ArrayOfProducts/Tests.cs
@@ -0,0 +1,24 @@
+namespace CodingExercise.ArrayOfProducts
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestA()
+        {
+            // arrange:
+            int[] array = { 5, 1, 4, 2 };
+
+            // 8 is equal to 1 x 4 x 2
+            // 40 is equal to 5 x 4 x 2
+            // 10 is equal to 5 x 1 x 2
+            // 20 is equal to 5 x 1 x 4
+            int[] expectedOutput = { 8, 40, 10, 20 };
+
+            // act:
+            var result = Exercise.ArrayOfProducts(array);
+
+            // assert:
+            result.Should().BeEquivalentTo(expectedOutput);
+        }
+    }
+}
