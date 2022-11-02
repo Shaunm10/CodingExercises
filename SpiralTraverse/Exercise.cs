58328b72be0b709c05769dcffbaf3da4361ef1b1
shaunsargent <ShaunM10@gmail.com>
2022-11-01 22:05:28 -0400

    SpiralTraverse WIP

--- SpiralTraverse/Exercise.cs
+++ SpiralTraverse/Exercise.cs
@@ -16,41 +16,99 @@ namespace CodingExercises.SpiralTraverse
             var processedItems = new List<Point>();
             var xIndex = 0;
             var yIndex = 0;
-            var direction = Direction.Left;
+            var direction = Direction.Right;
 
-            // assuming this is a square 2 dimensioal array.
+            // assuming this is a square 2 dimensional array.
             var countOfOneSide = array.Length;
             var totalCountOfAllItems = Math.Pow(countOfOneSide, 2);
 
             for (int i = 0; i < totalCountOfAllItems; i++)
             {
                 // get the number at these coordinates and add them to the list.
-                var numberAtIndexes = array[xIndex, yIndex];
+                var numberAtIndexes = array[yIndex, xIndex];
                 items.Add(numberAtIndexes);
+
+                // record this space
+                processedItems.Add(new Point { X = xIndex, Y = yIndex });
 
                 switch (direction)
                 {
                     case Direction.Left:
-
-                        // can we keep going left?
-                        if (array.Length < yIndex)
+                        if (IsHittingBarrier(array, processedItems, xIndex - 1, yIndex))
                         {
-
+                            // change direction
+                            direction = Direction.Up;
+                            yIndex--;
                         }
+                        else
+                        {
+                            xIndex--;
+                        }
+
                         break;
 
                     case Direction.Down:
+                        // can we keep going down?
+                        if (IsHittingBarrier(array, processedItems, xIndex, yIndex - 1))
+                        {
+                            // change direction
+                            xIndex--;
+                            direction = Direction.Left;
+                        }
+                        else
+                        {
+                            yIndex--;
+                        }
                         break;
 
                     case Direction.Right:
+                        // can we keep going right?
+                        if (IsHittingBarrier(array, processedItems, xIndex + 1, yIndex))
+                        {
+                            // change direction
+                            yIndex--;
+                            direction = Direction.Down;
+                        }
+                        else
+                        {
+                            xIndex++;
+                        }
                         break;
 
                     case Direction.Up:
+
+                        // can we keep going Up?
+                        if (IsHittingBarrier(array, processedItems, xIndex, yIndex - 1))
+                        {
+                            // change direction
+                            xIndex++;
+                            direction = Direction.Right;
+                        }
+                        else
+                        {
+                            yIndex--;
+                        }
                         break;
                 }
             }
 
             return items;
+        }
+
+        private static bool IsHittingBarrier(int[,] array, List<Point> processedItems, int xIndex, int yIndex)
+        {
+            if (array.Length > yIndex || array.GetLength)
+
+                if (array[xIndex, yIndex] == null)
+                {
+                    return true;
+                }
+
+            if (processedItems.Any(p => p.X == xIndex && p.Y == yIndex))
+            {
+                return true;
+            }
+            return false;
         }
     }
 
