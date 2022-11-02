aa5b2be92c27e0e620485c680f5305bca230c41b
Shaun Sargent <ssargent@costar.com>
2022-11-02 09:42:02 -0400

    LongestPeak setup/

---
+++ LongestPeak/Tests.cs
@@ -0,0 +1,19 @@
+namespace CodingExercises.LongestPeak
+{
+    public class Tests
+    {
+        public void TestA()
+        {
+            // arrange:
+            //var array = new int[] { 1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3 };
+            int[] array = { 1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3 };
+            var expectedOutput = 6; // 0, 10, 6, 5, -1, -3
+
+            // act:
+            var result = Exercise.LongestPeak(array);
+
+            // assert:
+            result.Should().Be(expectedOutput);
+        }
+    }
+}
