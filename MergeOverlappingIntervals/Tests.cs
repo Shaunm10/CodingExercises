b1120a53dae09bd24711a6ebb60ac3c3ed2b1e43
Shaun Sargent <ssargent@costar.com>
2022-11-03 16:32:36 -0400

    added shell for MergeOverlappingIntervals

---
+++ MergeOverlappingIntervals/Tests.cs
@@ -0,0 +1,31 @@
+namespace CodingExercises.MergeOverlappingIntervals
+{
+    public class Tests
+    {
+        [Fact]
+        public void testA()
+        {
+            // arrange:
+            int[][] intervals = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 3, 5 },
+                new int[] { 4, 7 },
+                new int[] { 6, 8 },
+                new int[] { 9, 10 }
+            };
+
+            var expectedOutput = new int[][]
+            {
+                new int[] {1,2},
+                new int[] {3,8},
+                new int[] {9,10}
+            };
+
+            // act:
+            var result = Exercise.MergeOverlappingIntervals(intervals);
+
+            // assert:
+        }
+    }
+}
