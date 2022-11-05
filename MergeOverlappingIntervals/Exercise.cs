6ad6ff8919d6f86212c7ac640d8d8c0354c2b7d3
Shaun Sargent <ssargent@costar.com>
2022-11-05 14:30:01 -0400

    Working on outlining MergeOverlappingIntervals

--- MergeOverlappingIntervals/Exercise.cs
+++ MergeOverlappingIntervals/Exercise.cs
@@ -18,8 +18,58 @@ namespace CodingExercises.MergeOverlappingIntervals
         */
         public static int[][] MergeOverlappingIntervals(int[][] intervals)
         {
+            var uniqueIntervals = new int[][] { };
+
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                var interval = intervals[0];
+
+                int[] overlappingIndexes = GetOverlappingIntervalIndexes(uniqueIntervals, interval);
+
+                if (overlappingIndexes.Length == 0)
+                {
+                    // add this interval to the list.
+                    uniqueIntervals = uniqueIntervals.Append(interval).ToArray();
+
+                }
+                else
+                {
+                    // combine them into 1 big interval
+                    int[] combinedInterval = CombineIntervals(uniqueIntervals, overlappingIndexes, interval);
+
+                    // remove the exiting overlapping ones.
+                    RemoveIntervals(uniqueIntervals, overlappingIndexes);
+
+                    // add this new combined one.
+                    uniqueIntervals = uniqueIntervals.Append(combinedInterval).ToArray();
+
+                }
+            }
+
             // Write your code here.
-            return new int[][] { };
+            return uniqueIntervals;
+        }
+
+        private static void RemoveIntervals(int[][] uniqueIntervals, int[] overlappingIndexes)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int[] CombineIntervals(int[][] uniqueIntervals, int[] overlappingIndexes, int[] interval)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int[] GetOverlappingIntervalIndexes(int[][] uniqueIntervals, int[] interval)
+        {
+            var overlappingIndexes = new int[] { };
+
+            for(var i =0; i< uniqueIntervals.Length; i++)
+            {
+                
+            }
+
+            return overlappingIndexes;
         }
     }
 }
