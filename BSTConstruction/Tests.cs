d15c766c8395c84913f20952fde96c78aacc14f5
shaunsargent <ShaunM10@gmail.com>
2022-11-06 15:19:22 -0500

    - added more test.

--- BSTConstruction/Tests.cs
+++ BSTConstruction/Tests.cs
@@ -49,5 +49,31 @@ namespace CodingExercises.BSTConstruction
             result.Should().Be(expectedResult);
 
         }
+
+        [Fact]
+        public void Remove()
+        {
+            // arrange;
+            var root = new BST(10);
+            root.Insert(5);
+            root.Insert(15);
+            root.Insert(2);
+            root.Insert(5);
+            root.Insert(13);
+            root.Insert(22);
+            root.Insert(1);
+            root.Insert(14);
+            var expectedResult = false;
+            var expectedResult2 = true;
+
+            // act:
+            var newRoot = root.Remove(10);
+            var result = newRoot.Contains(10);
+            var result2 = newRoot.Contains(15);
+
+            // assert:
+            result.Should().Be(expectedResult);
+            result2.Should().Be(expectedResult2);
+        }
     }
 }
