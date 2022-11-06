2391f26b22841f8bd8d3f174c589b353955d93b2
shaunsargent <ShaunM10@gmail.com>
2022-11-06 15:07:57 -0500

    Added InsertFalse

--- BSTConstruction/Tests.cs
+++ BSTConstruction/Tests.cs
@@ -25,5 +25,29 @@ namespace CodingExercises.BSTConstruction
             result.Should().Be(expectedResult);
 
         }
+
+        [Fact]
+        public void InsertFalse()
+        {
+            // arrange;
+            var root = new BST(10);
+            root.Insert(5);
+            root.Insert(15);
+            root.Insert(2);
+            root.Insert(5);
+            root.Insert(13);
+            root.Insert(22);
+            root.Insert(1);
+            root.Insert(14);
+            var expectedResult = false;
+
+            // act:
+            root.Insert(27);
+            var result = root.Contains(12);
+
+            // assert:
+            result.Should().Be(expectedResult);
+
+        }
     }
 }
