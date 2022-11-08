95333afc9203b55a9ec1989b86d45a3e282413f9
shaunsargent <ShaunM10@gmail.com>
2022-11-07 21:39:16 -0500

    -wip

--- ValidateBST/BST.cs
+++ ValidateBST/BST.cs
@@ -10,4 +10,9 @@ public class BST
     {
         this.value = value;
     }
+
+    public BST()
+    {
+
+    }
 }
--- ValidateBST/Tests.cs
+++ ValidateBST/Tests.cs
@@ -3,4 +3,26 @@ namespace CodingExercises.ValidateBST;
 public class Tests
 {
 
+    [Fact]
+    public void TestA()
+    {
+        // arrange:
+        var root = new BST
+        {
+            value = 10,
+            left = new BST
+            {
+                value = 5
+            },
+            right = new BST
+            {
+                value= 15
+            }
+        };
+
+
+        // act:
+
+        // assert:
+    }
 }
