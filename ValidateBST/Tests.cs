63f11b8b0f4f0d29ee9017f19559d97423d2419a
Shaun Sargent <ssargent@costar.com>
2022-11-08 08:03:29 -0500

    Added Failing Test1

--- ValidateBST/Tests.cs
+++ ValidateBST/Tests.cs
@@ -122,4 +122,54 @@ public class Tests
         // assert:
         result.Should().Be(false);
     }
+
+    [Fact]
+    public void Test1()
+    {
+        // arrange:
+        var root = new BST
+        {
+            value = 10,
+            left = new BST
+            {
+                value = 5,
+                left = new BST
+                {
+                    value = 2,
+                    left = new BST
+                    {
+                        value = 1
+                    }
+                },
+                right = new BST
+                {
+                    value = 5
+                }
+
+            },
+            right = new BST
+            {
+                value = 15,
+                left = new BST
+                {
+                    value = 13,
+                    right = new BST
+                    {
+                        value = 14
+
+                    }
+                },
+                right = new BST
+                {
+                    value = 22
+                }
+            }
+        };
+
+        // act:
+        var result = Exercise.ValidateBst(root);
+
+        // assert:
+        result.Should().Be(true);
+    }
 }
