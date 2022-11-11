3ef9e7dc580eca420b006f7a3519b56c82428f42
Shaun Sargent <ssargent@costar.com>
2022-11-11 08:28:46 -0500

    adding Unit test

--- MinHeightBST/Tests.cs
+++ MinHeightBST/Tests.cs
@@ -2,6 +2,16 @@ namespace CodingExercises.MinHeightBST
 {
     public class Tests
     {
-        
+        public void TestA()
+        {
+            // arrange:
+            var array = new List<int> { 1, 2, 5, 7, 10, 13, 14, 15, 22 };
+
+            // act:
+            var result = Exercise.MinHeightBst(array);
+
+            // assert: TBD
+
+        }
     }
 }
