44dc29265c754e888822f91d47e05a2ae4cace92
Shaun Sargent <shaunm10@gmail.com>
2022-11-13 11:34:31 -0500

    Added basic unit test.

--- ReconstructBST/Tests.cs
+++ ReconstructBST/Tests.cs
@@ -3,4 +3,16 @@ namespace CodingExercises.ReconstructBST;
 public class Tests
 {
 
+    [Fact]
+    public void TestA()
+    {
+        // arrange:
+        var input = new List<int> { 10, 4, 2, 1, 5, 17, 19, 18 };
+
+        // act:
+        var result = new Exercise().ReconstructBst(input);
+
+        // assert:
+        result.Should().NotBeNull();
+    }
 }
