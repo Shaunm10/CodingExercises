ce8683746dd0cb22504dcf5fe14f6275e1164da6
shaunsargent <ShaunM10@gmail.com>
2022-11-14 07:15:35 -0500

    - added another test.

--- ReconstructBST/Tests.cs
+++ ReconstructBST/Tests.cs
@@ -8,7 +8,7 @@ public class Tests
     {
         // arrange:
         var input = new List<int> { 10, 4, 2, 1, 5, 17, 19, 18 };
-        var expectedBStOutput = new BST
+        var expectedBstOutput = new BST
         {
             value = 10,
             left = new BST
@@ -40,7 +40,7 @@ public class Tests
                 }
             }
         };
-        var expectedOutputJson = JsonSerializer.Serialize(expectedBStOutput, new JsonSerializerOptions
+        var expectedOutputJson = JsonSerializer.Serialize(expectedBstOutput, new JsonSerializerOptions
         {
             WriteIndented = true
         });
@@ -51,6 +51,52 @@ public class Tests
         {
             WriteIndented = true
 
+        });
+
+        // assert:
+        result.Should().NotBeNull();
+        resultJson.Should().BeEquivalentTo(expectedOutputJson);
+    }
+
+    [Fact]
+    public void TestB()
+    {
+        // arrange:
+        var input = new List<int> { 2, 0, 1, 4, 3, 3 };
+        var expectedBstOutput = new BST
+        {
+            value = 2,
+            left = new BST
+            {
+                value = 0,
+                right = new BST
+                {
+                    value = 1
+                }
+            },
+            right = new BST
+            {
+                value = 4,
+                left = new BST
+                {
+                    value = 3,
+                    left = new BST
+                    {
+                        value = 3
+                    }
+                }
+            }
+        };
+        var expectedOutputJson = JsonSerializer.Serialize(expectedBstOutput, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        // act:
+        var result = new Exercise().ReconstructBst(input);
+        var resultJson = JsonSerializer.Serialize(result, new JsonSerializerOptions
+        {
+            WriteIndented = true
         });
 
         // assert:
