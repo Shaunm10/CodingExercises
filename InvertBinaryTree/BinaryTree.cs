e06eb7f83e126400e7acb6110e1de3fc0b5ced98
shaunsargent <ShaunM10@gmail.com>
2022-11-18 06:43:59 -0500

    updated InvertBinaryTree test

--- InvertBinaryTree/BinaryTree.cs
+++ InvertBinaryTree/BinaryTree.cs
@@ -3,12 +3,17 @@ namespace CodingExercises.InvertBinaryTree
     public class BinaryTree
     {
         public int value;
-        public BinaryTree left;
-        public BinaryTree right;
+        public BinaryTree? left;
+        public BinaryTree? right;
 
         public BinaryTree(int value)
         {
             this.value = value;
+        }
+
+        public BinaryTree()
+        {
+            this.value = 0;
         }
     }
 }
--- InvertBinaryTree/Tests.cs
+++ InvertBinaryTree/Tests.cs
@@ -6,13 +6,65 @@ namespace CodingExercises.InvertBinaryTree
         public void TestA()
         {
             // arrange:
-            
+            var binaryTree = new BinaryTree
+            {
+                value = 1,
+                left = new BinaryTree
+                {
+                    value = 2,
+                    left = new BinaryTree
+                    {
+                        value = 4,
+                        right = new BinaryTree
+                        {
+                            value = 9
+                        },
+                        left = new BinaryTree
+                        {
+                            value = 8
+                        }
+                    },
+                    right = new BinaryTree
+                    {
+                        value = 5
+                    }
+                },
+                right = new BinaryTree
+                {
+                    value = 3,
+                    left = new BinaryTree
+                    {
+                        value = 6
+                    },
+                    right = new BinaryTree
+                    {
+                        value = 7
+                    }
+                }
+            };
+
+
 
             // act:
-            Exercise.InvertBinaryTree(null);
+            Exercise.InvertBinaryTree(binaryTree);
 
             // assert:
-            //result.should().NotBeNull();
+            binaryTree.value.Should().Be(1);
+            binaryTree.left.value.Should().Be(3);
+            binaryTree.left.left.value.Should().Be(7);
+            binaryTree.left.right.value.Should().Be(6);
+
+
+            binaryTree.right.value.Should().Be(2);
+            binaryTree.right.left.value.Should().Be(5);
+            binaryTree.right.right.value.Should().Be(4);
+            binaryTree.right.right.left.value.Should().Be(9);
+            binaryTree.right.right.right.value.Should().Be(8);
+
+
+
+
+
         }
     }
 }
